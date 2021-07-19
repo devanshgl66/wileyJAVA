7e90a0b85bd4d69c1d79bf2c9d908f1df001b74e
devanshgl66 <devanshgl55@gmail.com>
2021-07-19 10:14:07 +0530

    basic linkedlist

---
+++ src/dataStructure/linkedListBasic.java
@@ -0,0 +1,51 @@
+package dataStructure;
+
+class ll<T>{
+	T data;
+	ll<T> next;
+	ll(T data,ll<T>link){
+		this.data=data;
+		this.next=link;
+	}
+}
+class linkedlist{
+	ll<Integer> head=null;
+	void insertInBeg(int data) {
+		if(head==null)
+			head=new ll<Integer>(data,null);
+		else {
+			ll<Integer>newHead=new ll<Integer>(data,head);
+			head=newHead;
+		}
+	}
+	int removeFromHead() {
+		if(head==null) {
+			System.out.println("List already empty");
+			return -1;
+		}
+		int d=head.data;
+		head=head.next;
+		return d;		
+	}
+	void traverse() {
+		ll<Integer>temp=head;
+		while(temp!=null) {
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+	}
+}
+public class linkedListBasic {
+	public static void main(String[] args) {
+//		ll<Integer>head=new ll<Integer>(0,null);
+		linkedlist li=new linkedlist();
+		li.insertInBeg( 5);
+		li.insertInBeg(6);
+		li.insertInBeg( 7);
+		System.out.println("First traversal");
+		li.traverse();
+		li.removeFromHead();
+		System.out.println("Traverse after removal of element from head");
+		li.traverse();
+	}
+}
